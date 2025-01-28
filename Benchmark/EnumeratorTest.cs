9a6efeb4070a28b10472f74bd30c0c7b2f8e02b6
Gabriel Marinho <gabrielmarinho2007@gmail.com>
2025-01-28 20:58:11 -0300

    Initial commit

---
+++ Benchmark/EnumeratorTest.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark;
+
+public class EnumeratorTest
+{
+    private IEnumerable<int> AuxYield()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            yield return i;
+        }
+    }
+
+    [Benchmark()]
+    public void ComYield()
+    {
+        foreach (var value in AuxYield())
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    private IEnumerable<int> AuxSemYield()
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < 100; i++)
+        {
+            list.Add(i);
+        }
+
+        return list;
+    }
+    
+    [Benchmark()]
+    public void SemYield()
+    {
+        var list = AuxSemYield();
+        foreach (var value in list)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+}
---
+++ Benchmark/MyClassToTest.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+
+public class MyClassToTest {
+
+    private const string MyMessage = "Hello World!";
+    
+    public async Task<string> MyMethodWithAwaitAsync() {
+        return await Task.FromResult(MyMessage);
+    }
+	
+    public Task<string> MyMethodWithoutAwaitAsync() {
+        return Task.FromResult(MyMessage);
+    }
+	
+    [Benchmark]
+    public async Task<string> ReturnWithAwaitAsync() => await MyMethodWithAwaitAsync();
+	
+    [Benchmark]
+    public Task<string> ReturnWithoutAwaitAsync() => MyMethodWithoutAwaitAsync();
+	
+}
