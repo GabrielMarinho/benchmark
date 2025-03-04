39931fd087981035cd6162dc95c0e6f686fcbd4e
Gabriel Marinho <gabrielmarinho2007@gmail.com>
2025-03-04 10:12:55 -0300

    Adding Substring test

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Benchmark;
+using Benchmark.Tests;
 using BenchmarkDotNet.Running;
 
-var summary = BenchmarkRunner.Run<MyClassToTest>();
+var summary = BenchmarkRunner.Run<SubstringTest>();
---
+++ Benchmark/Tests/SubstringTest.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark.Tests;
+
+[MemoryDiagnoser]
+public class SubstringTest
+{
+
+    private readonly string _myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    
+    
+    [Benchmark]
+    public string SubstringWithSpanStart()
+    {
+        return _myString.AsSpan(0, 10).ToString();
+    }
+
+    [Benchmark]
+    public string SubstringWithStartAndLength()
+    {
+        return _myString[..10];
+    }
+    
+    [Benchmark]
+    public string SubstringWithSpanMiddle()
+    {
+        return _myString.AsSpan(20, 5).ToString();
+    }
+    
+    [Benchmark]
+    public string SubstringWithStartAndLengthMiddle()
+    {
+        return _myString[20..25];
+    }
+    
+    [Benchmark]
+    public string SubstringWithSpanEnd()
+    {
+        return _myString.AsSpan(45).ToString();
+    }
+    
+    [Benchmark]
+    public string SubstringWithStartAndLengthEnd()
+    {
+        return _myString[45..];
+    }
+
+}
